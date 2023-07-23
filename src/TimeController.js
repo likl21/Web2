c3930a7a1f75d55fbb32dfa364352a16d1c7978a
niehy21 <niehy21.thu@gmail.com>
2023-07-23 12:04:45 +0800

    Merged drag.js into TimeController.js, now dragging is available

--- src/TimeController.js
+++ src/TimeController.js
@@ -7,13 +7,20 @@
     var alarm = 0;
     var audio = document.createElement("audio");
     var isUpdating = false;
+    var isNeedPrecision = false;
     audio.src = "../resources/AlarmSound.mp3";
     audio.loop = true; 
     // 用于设置时间
+    const clock = document.getElementById("clock");
     const minuteHand = document.getElementById("minutehand");
     const hourHand = document.getElementById("hourhand");
     const secondHand = document.getElementById("secondhand");
-    
+    var isSecondHandSelected = false;
+    var isMinuteHandSelected = false;
+    var isHourHandSelected = false;
+    var initialState = "CLOCK";
+    var min_count = 0;
+
     // 用于表针动画
     const mHand = document.getElementById("mhand");
     const sHand = document.getElementById("shand");
@@ -129,6 +136,15 @@
         var timesAngle = getAngle();
         changeClockDisplay(timesAngle);
     }
+    function updatePreciseClockDisplay()
+    {
+        var timesAngle = getAngle();
+        var deltaTime = window.ButtonManager.getMorePrecision();
+        timesAngle.hour   += deltaTime / 60000.0;
+        timesAngle.minute += deltaTime * 0.001;
+        timesAngle.second += deltaTime * 0.06;
+        changeClockDisplay(timesAngle);
+    }
     function setTimeTo(seconds){
         if(seconds ==null){
             seconds = 0;
@@ -219,11 +235,251 @@
         isUpdating =false;
         return;
     }
-        
-    function changeStateTo(state){
-        if(currentState==state){
-            return;
+
+    // 鼠标按下事件处理程序
+    function handleMouseDown(event, element) {
+        event.preventDefault();
+        // element.classList.add('selected'); // 添加选中样式
+        //usedTime = initialHourAngle * 120 + initialMinuteAngle * 10 + initialSecondAngle / 6;
+        // document.getElementById("usedTime_value").textContent = usedTime;
+        initialState = getCurrentState();
+        if (element === secondHand) {
+            isSecondHandSelected = true;
+            isMinuteHandSelected = false;
+            isHourHandSelected = false;
+            changeStateTo("STOP");
+            //initialSecondAngle = Math.atan2(mouseY - centerY, mouseX - centerX);
+        } else if (element === minuteHand) {
+            isMinuteHandSelected = true;
+            isSecondHandSelected = false;
+            isHourHandSelected = false;
+            changeStateTo("STOP");
+            //initialMinuteAngle = Math.atan2(mouseY - centerY, mouseX - centerX);
+        } else if (element === hourHand) {
+            isHourHandSelected = true;
+            isSecondHandSelected = false;
+            isMinuteHandSelected = false;
+            changeStateTo("STOP");
+            //initialHourAngle = Math.atan2(mouseY - centerY, mouseX - centerX);
         }
+    }
+    // 鼠标松开事件处理程序
+    function handleMouseUp() {
+        // secondHand.classList.remove('selected');
+        // minuteHand.classList.remove('selected');
+        // hourHand.classList.remove('selected');
+        isSecondHandSelected = false;
+        isMinuteHandSelected = false;
+        isHourHandSelected = false;
+        window.TimeManager.changeStateTo(initialState);
+    }
+    var hour_flag = false;
+    var hour_flag_reverse = false;
+    function setDragHourTo(hourNum) {
+        var time_t = (getCurrentTime() + 86400) % 86400;
+        let hours = Math.floor(time_t / 3600);
+        let minutes = Math.floor((time_t - (hours * 3600)) / 60);
+        let seconds = time_t - (hours * 3600) - (minutes * 60);
+        if(hours <= 11){
+            hours = Math.max(0, Math.min(Math.floor(hourNum), 24));
+        }
+        else{
+            hours = (hourNum + 12) % 24;
+        }
+        setTimeTo((hours * 3600) + (minutes * 60) + seconds);
+        updateTimeDisplay();
+        updateClockDisplay();
+        if (hourNum === 0 && hour_flag === true) {
+            var temp_times = getTimeNum();
+            let pm_count = 0;
+            if(temp_times.hour < 12){
+                temp_times.hour = 12;
+            }
+            else{
+                temp_times.hour = 0;
+            }
+            hour_flag = false;
+            pm_count = temp_times.hour * 3600 + temp_times.second + temp_times.minute * 60;
+            setTimeTo(pm_count);
+            // alert(usedTime);
+            updateTimeDisplay();
+            updateClockDisplay();
+        }
+        if (hourNum === 11 && hour_flag_reverse === true) {
+            var temp_times = getTimeNum();
+            let pm_count = 0;
+            if(temp_times.hour < 12){
+                temp_times.hour = 23;
+            }
+            else{
+                temp_times.hour = 11;
+            }
+            hour_flag = false;
+            pm_count = temp_times.hour * 3600 + temp_times.second + temp_times.minute * 60;
+            setTimeTo(pm_count);
+            updateTimeDisplay();
+            updateClockDisplay();
+        }
+        hour_flag = false;
+        hour_flag_reverse = false;
+        if (hourNum === 11) {
+            hour_flag = true;
+        }
+        if (hourNum === 0) {
+            hour_flag_reverse = true;
+        }
+    }
+    var min_flag = false;
+    var min_flag_reverse = false;
+    var total_count = 0;
+    function setDragMinuteTo(minuteNum) {
+        var time_t = (getCurrentTime() + 86400) % 86400;
+        let hours = Math.floor(time_t / 3600);
+        let minutes = Math.floor((time_t - (hours * 3600)) / 60);
+        let seconds = time_t - (hours * 3600) - (minutes * 60);
+        minutes = Math.max(0, Math.min(Math.floor(minuteNum), 60));
+        setTimeTo((hours * 3600) + (minutes * 60) + seconds);
+        updateTimeDisplay();
+        updateClockDisplay();
+        if (minuteNum === 0 && min_flag === true) {
+            var temp_times = getTimeNum();
+            total_count = (temp_times.hour + 1+ 24)%24 * 3600;
+            min_flag = false;
+            total_count = total_count + temp_times.second;
+            setTimeTo(total_count);
+            updateTimeDisplay();
+            updateClockDisplay();
+        }
+        if (minuteNum === 59 && min_flag_reverse === true) {
+            var temp_times = getTimeNum();
+            total_count = (temp_times.hour + 24) % 24 * 3600;
+            min_flag_reverse = false;
+            total_count = total_count + temp_times.second - 60;
+            setTimeTo(total_count);
+            updateTimeDisplay();
+            updateClockDisplay();
+        }
+        min_flag = false;
+        min_flag_reverse = false;
+        if (minuteNum === 59) {
+            min_flag = true;
+        }
+        if (minuteNum === 0) {
+            min_flag_reverse = true;
+        }
+    }
+    var sec_flag = false;
+    var sec_flag_reverse = false;
+    var sec_count = 0;
+    function setDragSecondTo(secondNum) {
+        var time_t = (getCurrentTime() + 86400) % 86400;
+        let hours = Math.floor(time_t / 3600);
+        let minutes = Math.floor((time_t - (hours * 3600)) / 60);
+        let seconds = time_t - (hours * 3600) - (minutes * 60);
+        seconds = Math.max(0, Math.min(Math.floor(secondNum), 60));
+        setTimeTo((hours * 3600) + (minutes * 60) + seconds);
+        updateTimeDisplay();
+        updateClockDisplay();
+        if (secondNum === 0 && sec_flag === true) {
+
+            var temp_times = getTimeNum();
+            sec_count = temp_times.minute * 60;
+            sec_flag = false;
+            sec_count = sec_count + 60 + temp_times.hour * 3600;
+            setTimeTo(sec_count);
+            updateTimeDisplay();
+            updateClockDisplay();
+        }
+        if (secondNum === 59 && sec_flag_reverse === true) {
+            var temp_times = getTimeNum();
+            sec_count = temp_times.minute * 60;
+            sec_flag_reverse = false;
+            sec_count = sec_count - 1 + temp_times.hour * 3600;
+            setTimeTo(sec_count);
+            updateTimeDisplay();
+            updateClockDisplay();
+        }
+
+        sec_flag = false;
+        sec_flag_reverse = false;
+        if (secondNum === 59) {
+            sec_flag = true;
+        }
+        if (secondNum === 0) {
+            sec_flag_reverse = true;
+        }
+    }
+
+    // 鼠标移动事件处理程序
+    function handleMouseMove(event) {
+        var rect = clock.getBoundingClientRect();
+        var centerX = rect.left + rect.width / 2;
+        var centerY = rect.top + rect.height / 2;
+        var mouseX = event.clientX;
+        var mouseY = event.clientY;
+
+        //var angles = getAngle();
+        //var initialSecondAngle = -Math.PI / 2;
+        //var initialMinuteAngle = -Math.PI / 2;
+        //var initialHourAngle = -Math.PI / 2;
+        //关于这部分，考虑到使用usedTIme来表示时间，那么就不能使用连续值，因为连续值的使用，
+        //比如转动分针，要求秒针不转动，则必然小数上会产生余数，导致秒针转动，所以采用的是这种方式
+        if (isSecondHandSelected) {
+            var angle = Math.atan2(mouseY - centerY, mouseX - centerX);
+            var rotation = (Math.round((angle + Math.PI / 2) * 60 / (2 * Math.PI)) + 60) % 60;
+            //usedTime += rotation;
+
+            // document.getElementById('rotationValue').textContent = rotation.toFixed(2);
+            //updateTimeDisplay();
+            //updateClockDisplay();
+            setDragSecondTo(rotation);
+        }
+
+        if (isMinuteHandSelected) {
+            var angle = Math.atan2(mouseY - centerY, mouseX - centerX);
+            var rotation = (Math.round((angle + Math.PI / 2) * 60 / (2 * Math.PI)) + 60) % 60;
+            //usedTime += rotation * 60;
+            //updateTimeDisplay();
+            //updateClockDisplay();
+
+            setDragMinuteTo(rotation);
+
+            // document.getElementById('rotationValue').textContent = rotation.toFixed(2);
+
+        }
+
+        if (isHourHandSelected) {
+            var angle = Math.atan2(mouseY - centerY, mouseX - centerX);
+            var rotation = (Math.round((angle + Math.PI / 2) * 12 / (2 * Math.PI))+12)%12;
+            /*usedTime += rotation * 3600;
+            updateTimeDisplay();
+            updateClockDisplay();*/
+            // document.getElementById('rotationValue').textContent = rotation.toFixed(2);
+            setDragHourTo(rotation);
+        }
+        
+
+    }
+
+    // 事件监听
+    secondHand.addEventListener('mousedown', function (event) {
+        handleMouseDown(event, secondHand);
+    });
+    minuteHand.addEventListener('mousedown', function (event) {
+        handleMouseDown(event, minuteHand);
+    });
+    hourHand.addEventListener('mousedown', function (event) {
+        handleMouseDown(event, hourHand);
+    });
+    document.addEventListener('mouseup', function () {
+        handleMouseUp();
+    });
+    clock.addEventListener('mousemove', handleMouseMove);
+
+    // 初始更新时间和表盘显示
+
+    function changeStateTo(state){
+        
         currentState = state;
         switch(currentState){
             case "STOP":
@@ -237,8 +493,15 @@
             default:
                 break;
         }
-        updateTimeDisplay();
-        updateClockDisplay();
+        if(!isNeedPrecision)
+        {
+            updateClockDisplay();
+            isNeedPrecision = true;
+        }
+        else if(window.ButtonManager.getCurrentButtonState() == "STOPWATCH")
+            updatePreciseClockDisplay();
+        else
+            updateClockDisplay();
         switch(currentState)
         {
             case "UPDATE":
@@ -345,6 +608,9 @@
         changeClockDisplay: changeClockDisplay,
         //更新时钟指针至当前时间
         updateClockDisplay: updateClockDisplay,
+        //更新时钟指针至当前精确时间
+        updatePreciseClockDisplay: updatePreciseClockDisplay,
+        //isNeedPrecision: isNeedPrecision,
         //设置并播放正向动画
         runAnimation: runAnimation,
         //停止并删除正向动画
